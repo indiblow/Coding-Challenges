7e46f2272229ff173ab97505949a6bc6cd448287
Indigo <indigov4@gmail.com>
2021-01-14 20:30:58 -0800

    completed Coding Challenge 01/14

--- Jan14.js
+++ Jan14.js
@@ -1,1 +1,7 @@
-Jan 14 Coding Challenge
+// Jan 14 Coding Challenge
+
+let namesList = ['name1', 'name2', 'name3']
+function addName(name) {
+  namesList.push(name)
+  console.log(namesList.indexOf(name))
+}
